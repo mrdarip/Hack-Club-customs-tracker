053f86f30604f2886018e0a38656c010c3f3f543
mrdarip <darioelas2005@gmail.com>
2024-07-23 22:15:41 +0200

    implemented displaying products

--- js/searcher.js
+++ js/searcher.js
@@ -1,13 +1,34 @@
 var countries = [];
+var products = [];
 readAndSetCountries();
+readAndSetProducts();
 
-async function readAndSetCountries() {
-    await fetch("../data/countries.json").then(response => response.json()).then(data => {
-        console.log(data);
+ function readAndSetCountries() {
+    fetch("../data/countries.json").then(response => response.json()).then(data => {
         countries = data.countries;
-        console.log(countries);
         setCountries();
     });
+}
+
+function readAndSetProducts() {
+    fetch("../data/products.json").then(response => response.json()).then(data => {
+        products = data.productsAndImage;
+        setProducts();
+    });
+}
+
+function setProducts() {
+    var productsArea = document.getElementById("productsArea");
+    for (var i = 0; i < products.length; i++) {
+        var product = products[i];
+        var productDiv = document.createElement("div");
+
+        productDiv.className = "product";
+        productDiv.innerHTML = "<img src='" + product.image + "' alt='product image' width='100' height='100'>" +
+            "<p>" + product.name + "</p>";
+            
+        productsArea.appendChild(productDiv);
+    }
 }
 
 function setCountries() {
@@ -16,7 +37,6 @@ function setCountries() {
         var option = document.createElement("option");
         option.text = countries[i].name;
         option.value = countries[i].code;
-        console.log(countries[i].name);
         country.add(option);
     }
 }
